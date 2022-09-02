696be07c2518f77e8dbc76871aaa5ca043ac3476
sathiyapriyakm <sathiyapriya.km@gmail.com>
2022-09-02 12:21:38 +0530

    change in header font

--- src/components/studentDashboard/CardDashboard.js
+++ src/components/studentDashboard/CardDashboard.js
@@ -6,8 +6,7 @@ export function CardDashboard({detail,value, symbol}){
                <div className="card-body">
                    <div className="row no-gutters align-items-center">
                        <div className="col mr-2">
-                           <div className="text-m font-weight-bold text-info text-uppercase mb-1">
-                               {detail}</div>
+                       <h6 className="m-0 font-weight-bold text-info">{detail}</h6>
                            <div className="h5 mb-0 font-weight-bold text-gray-800">{value}</div>
                        </div>
                        <div className="col-auto">
